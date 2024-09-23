9650bc7530b43d22643ff45017ac5576f36b5ce9
Luiz Felippe <123516499+luizfelippe-dev@users.noreply.github.com>
2024-09-23 18:28:11 -0300

    Add files via upload

--- CleanAutomate.py
+++ CleanAutomate.py
@@ -1,18 +1,20 @@
 #Automação de Limpeza de Backups
-"""BrainStorm: Nome, Data, Tamanho, Copia
+"""BrainStorm:
 
-Verificar Nome, Data Do Backup, Tamanho do arquivo sempre maior que o anterior.
- Validar se já possui copias do arquivo
- Sempre mantes 1 SW mais atual
+Caminho das Pastas: \\10.1.2.35\backup\clientes\2023\1\ARH\C_AGUACLARA
+
  ==3 arquivos por mes nos anos de 2020 a 2022
  ==1 arquivo por dia nos anos de 2023 a 2024
- verificação de funcionamento do banco, descompactando
+
+ Entrar na pasta (Filho), fazer o processo. Ao Finalizar, retornar para pasta anterior(Pai) e acessar a pasta seguinte(Filho2).
  
- Validações: Duplicata com mesmo nome 
- - verificar data mantendo apenas um banco por dia (Se o ano for ==2023 && ==2024)
- - verificar horario mais perto do fim do dia 
+ Validações: Verificar se à dois arquivos com mesmo nome 
+ - verificar data do arquivo duplicado mantendo apenas um banco por dia
+ - verificar qual tem o horario mais perto do fim do dia 
  - verificar tamanho do arquivo priorizando sempre o maior
- - no caso dos arquivos serem absolutamente iguais excluir duplicatas e manter apenas 1 arquivo 
+ - Priorizar arquivo que possui SW no nome se passar pelas validações anteriores (Data/horario e tamanho)
+ - na verificação de tamanho do arquivo, fazer média de todos os arquivos, os arquivos que tiver metade do tamanho da media dos arquivos devem ser deletados
+ - no caso dos arquivos serem absolutamente iguais excluir duplicatas e manter apenas 1 arquivo
  
  - Testar a automação inserindo a letra "Z" no começo ou final do nome do arquivo
  - No caso de funcionar apenas trocar a função para que exclua os arquivos
@@ -20,5 +22,5 @@ Verificar Nome, Data Do Backup, Tamanho do arquivo sempre maior que o anterior.
  ----------------------------------------------------------------------------------
 automação teste para adicionar "Z" dentro do nome dos backups e outra para retirar oq foi inserido.
  ----------------------------------------------------------------------------------
- - possibilidade de criar uma automação de inserção de caracteres predefinidos nos backups
+ - possibilidade de criar uma automação de inserção de caracteres predefinidos nos arquivos
    e outra automação apenas para validar e excluir arquivos com os caracteres."""
