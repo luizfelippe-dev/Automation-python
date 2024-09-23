db2648566216783a885a34935bc67a914d7f0492
Luiz Felippe <123516499+luizfelippe-dev@users.noreply.github.com>
2024-09-23 18:45:35 -0300

    Add files via upload

--- CleanAutomate.py
+++ CleanAutomate.py
@@ -8,12 +8,13 @@ Caminho das Pastas: \\10.1.2.35\backup\clientes\2023\1\ARH\C_AGUACLARA
 
  Entrar na pasta (Filho), fazer o processo. Ao Finalizar, retornar para pasta anterior(Pai) e acessar a pasta seguinte(Filho2).
  
- Validações: Verificar se à dois arquivos com mesmo nome 
- - verificar data do arquivo duplicado mantendo apenas um banco por dia
- - verificar qual tem o horario mais perto do fim do dia 
- - verificar tamanho do arquivo priorizando sempre o maior
- - Priorizar arquivo que possui SW no nome se passar pelas validações anteriores (Data/horario e tamanho)
+ Validações:
+
  - na verificação de tamanho do arquivo, fazer média de todos os arquivos, os arquivos que tiver metade do tamanho da media dos arquivos devem ser deletados
+ - Verificar se à dois arquivos com a mesma data, Se sim ir para proxima validação, se não ir para proximo arquivo.zip
+ - verificar qual tem o horario mais perto do fim do dia, Horario mais perto de 23:59:59 Ter Prioridade e ir para proxima validação 
+ - verificar tamanho do arquivo priorizando sempre o maior, se for mesmo tamanho ir Excluir o mais antigo.
+ - Verificar nome do arquivo que possui SW no nome, se passar pelas validações anteriores (Data/horario e tamanho) então manter o arquivo SW como prioridade, e excluir os outros com mesma data e tamanho ou tamanho menores, caso contrario ir para proxima validação.
  - no caso dos arquivos serem absolutamente iguais excluir duplicatas e manter apenas 1 arquivo
  
  - Testar a automação inserindo a letra "Z" no começo ou final do nome do arquivo
