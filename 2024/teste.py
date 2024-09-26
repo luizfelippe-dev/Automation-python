a94128de2388eb686009d0aa6202c187247039bb
Julio Sales <83310641+Locked666@users.noreply.github.com>
2024-09-25 23:26:52 -0300

    Delete 2024 directory

--- 2024/teste.py
+++
@@ -1,5 +0,0 @@
-import os 
-import random
-for i in range(1000):
-    os.system(f"echo testetestestststesftssdfsdfsdfgbalkdfgjklfg >> FB3{random.choice(['','_SW'])}_INTEGRACAO_{random.randint(1,30)}_{random.randint(1,12)}_2024__{random.randint(1,24)}_5_5_FBK.zip")
-    
