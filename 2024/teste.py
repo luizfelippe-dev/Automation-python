3ff68c7a837a38ddbfb2c7aaffaff485492a89e4
Julio.sales <julio.cordeiro@qualitysistemas.local>
2024-09-26 07:36:42 -0400

    save finaly

--- 2024/teste.py
+++ 2024/teste.py
@@ -1,5 +1,5 @@
 import os 
 import random
-for i in range(100000):
+for i in range(1000):
     os.system(f"echo testetestestststesftssdfsdfsdfgbalkdfgjklfg >> FB3{random.choice(['','_SW'])}_INTEGRACAO_{random.randint(1,30)}_{random.randint(1,12)}_2024__{random.randint(1,24)}_5_5_FBK.zip")
     
